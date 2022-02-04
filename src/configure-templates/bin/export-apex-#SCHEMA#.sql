0f5a185707445701436ea5350c25913f26fcedf8
Dietmar Aust <dietmar.aust@opal-consulting.de>
2022-02-04 15:42:53 +0100

    added helpful instructions for exporting apex and rest applications

--- src/configure-templates/bin/export-apex-#SCHEMA#.sql
+++ src/configure-templates/bin/export-apex-#SCHEMA#.sql
@@ -1,17 +1,40 @@
 -----------------------------------------------------------------------------
 -- APEX export
--- you can turn debugging on using the switch: -debug 
 -----------------------------------------------------------------------------
 
 prompt *** Please configure the sql file for export
 prompt *** Currently, nothing is exported. 
 
+
 /*
-prompt *** exporting apex applications from the workspace
--- mit select * from apex_workspaces ermitteln
-apex export -workspaceid 1635506190835543 
+    To find the right command line switches, please try the commands in your locally installed SQLcl client first.
 
-prompt *** exporting apex application xxx
-apex export -applicationid 201 -expOriginalIds
+    If you have not yet installed it, you can download it from here: 
+        - https://www.oracle.com/tools/downloads/sqlcl-downloads.html
+    
+    - start sqlcl 
+        - (e.g. "sql user/pwd@localhost:1521/xe")
 
-*/ 
+    -------------------
+    -- APEX EXPORT
+    -------------------
+    "help apex": help information on the apex commands
+    "apex export": lists the command line options that are available
+    "apex list": lists all applications, including their ids, the workspaces and ids
+
+    Examples (always make sure you are connected with a schema that access to your workspace)
+    - Basic export of application 100:
+        - "apex export -applicationid 100"
+    - Typical command that we often use
+        - "apex export -applicationid 100 -expOriginalIds -expComments -expSupportingObjects Y -expACLAssignments -skipExportDate"
+        when you have translations: 
+        - "apex export -applicationid 100 -expOriginalIds -expComments -expSupportingObjects Y -expACLAssignments -skipExportDate -expTranslations"
+
+    - Splitting the application into multiple files for seeing differences using version control: 
+        - "apex export -applicationid 100 -expOriginalIds -expComments -expSupportingObjects Y -expACLAssignments -skipExportDate -split"
+
+    - Export of all applications in the current workspace
+        - "apex export -workspaceid 9999999"
+        - "apex export -workspaceid 9999999 -expOriginalIds -expComments -expSupportingObjects Y -expACLAssignments -skipExportDate"
+
+*/
--- src/configure-templates/bin/export-rest-#SCHEMA#.sql
+++ src/configure-templates/bin/export-rest-#SCHEMA#.sql
@@ -9,3 +9,29 @@ rest export
 prompt /
 
 spool off
+
+
+/*
+    To find the right command line switches, please try the commands in your locally installed SQLcl client first.
+
+    If you have not yet installed it, you can download it from here: 
+        - https://www.oracle.com/tools/downloads/sqlcl-downloads.html
+    
+    - start sqlcl 
+        - (e.g. "sql user/pwd@localhost:1521/xe")
+
+    -------------------
+    -- REST EXPORT
+    -------------------
+    "help rest": help information on the rest commands
+
+    - Typical export command: 
+
+    prompt *** spool rest modules
+    spool my_rest_modules_export.sql
+    rest export
+    prompt /
+    spool off
+
+*/
+
